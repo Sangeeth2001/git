432b6bb4a86127d38a0e772088e8939ad783b045
Ahamed0097 <ahmadhenoor23@gmail.com>
2023-12-21 12:50:50 +0530

    settings module changes

--- job provider/provider/src/app/settings/components/change-password/change-password.component.ts
+++ job provider/provider/src/app/settings/components/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent {
+  changepasswordForm: FormGroup;
+  show = false;
+  password !: string;
+
+  constructor(private formBuilder:FormBuilder){
+    this.changepasswordForm = this.formBuilder.group({
+      userid: ['', Validators.required],
+      oldpass: ['', Validators.required],
+      newpass: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      confirmpass: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]]
+    });
+  }
+
+  postData(){
+    console.log(this.changepasswordForm);
+  }
+
+  ngOnInit() {
+    this.password = 'password';
+  }
+
+  onClick() {
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.show = true;
+    } else {
+      this.password = 'password';
+      this.show = false;
+    }
+  }
 
 }
--- job provider/provider/src/app/settings/components/change-username/change-username.component.ts
+++ job provider/provider/src/app/settings/components/change-username/change-username.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-username',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-username.component.css']
 })
 export class ChangeUsernameComponent {
+
+  changeusernameForm! : FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+    this.changeusernameForm = this.formBuilder.group({
+      fname: ['', Validators.required],
+      uname: ['', Validators.required],
+      emailadd: ['', [Validators.required, Validators.email, Validators.minLength(5),Validators.minLength(10)]],
+      pnum: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]]
+    });
+  }
+
+  postData(){
+    console.log(this.changeusernameForm);
+  }
 
 }
