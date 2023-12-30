3e8cac9cf8c5aeee334edac73ad6deaa790b0cb3
Ahamed0097 <ahmadhenoor23@gmail.com>
2023-12-30 13:26:51 +0530

    updated

--- job provider/provider/src/app/auth/components/login/login.component.ts
+++ job provider/provider/src/app/auth/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router:Router){}
+  constructor(private router:Router , private authService:AuthService){}
   navigateTo()
   {
     this.router.navigate(['/provider-home/dashboard']);
   }
+
+  login(data:any){
+    this.authService.verifyLogin(data).subscribe((response: any)=>{
+      console.log(response);
+  }
+ )}
 }
