edba9db097674388e16908ec47e87a881fc8beeb
Sangeeth2001 <sangeethmu54@gmail.com>
2024-01-16 13:50:23 +0530

    changed

--- job provider/provider/src/app/shared/sidebar/sidebar.component.ts
+++ job provider/provider/src/app/shared/sidebar/sidebar.component.ts
@@ -2,21 +2,18 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/service/auth.service';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  constructor (private router: Router ,private service:AuthService) {
-    this.accessId=service.getid();
-  }
+  constructor (private router: Router ,private service:AuthService) {}
 
-  getId(): any {
-    return sessionStorage.getItem('accessid') // Return an empty string if the token is null or undefined
-  }
+  
 
-   accessId=this.getId();
+
 
   navigatetoDashboard(){
     this.router.navigate(['home/dashboard']);
