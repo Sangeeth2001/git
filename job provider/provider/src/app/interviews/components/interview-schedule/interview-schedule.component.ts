5cf28b8a4bdc8e922478435c2826d9b290f3bf31
abinktyz <abinkrishnaptb@gmail.com>
2024-01-23 13:56:05 +0530

    added

--- job provider/provider/src/app/interviews/components/interview-schedule/interview-schedule.component.ts
+++ job provider/provider/src/app/interviews/components/interview-schedule/interview-schedule.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ApplicationService } from 'src/app/application/service/application.service';
+import { applicationid } from 'src/app/application/model/applicant';
+import { applicants } from 'src/app/application/model/applicant';
+import { InterviewsService } from '../../service/interviews.service';
 @Component({
   selector: 'app-interview-schedule',
   templateUrl: './interview-schedule.component.html',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class InterviewScheduleComponent {
 
+  applicants!:applicants[];
+  applicationid!:applicationid[];
+  constructor (private as:ApplicationService, private interviewservice:InterviewsService){}
+
+  ngOnInit(){
+    this.listInterview();
+    this.listApplicants();
+  }
+
+  listApplicants(){
+    this.as.getApplication().subscribe((result:any)=>{
+      this.applicants=result;
+      console.log(result);
+      
+    })
+  }
+  removeInterviewlist(id:any){
+    this.interviewservice.removeInterview(id).subscribe((result:any)=>{
+      console.log(result);
+    });
+    this.listApplicants
+    
+  }
+
+  listInterview(){
+    this.as.getDatelist().subscribe((Response:any)=>{
+      this.applicationid=Response;
+    })
+  }
+  
 }
+
+
--- job provider/provider/src/app/interviews/service/interviews.service.ts
+++ job provider/provider/src/app/interviews/service/interviews.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { interview } from '../model/interview';
 import { environment } from 'src/app/environment/environments';
+import { applicants } from 'src/app/application/model/applicant';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class InterviewsService {
   postinterview(data:any){
     const jobProviderId=this.getId();
     return this.http.post<interview[]>(environment.baseurl+'Interview/company/company-user/'+jobProviderId+'/Interview',data)
+  }
+
+  removeInterview(id:any){
+    return this.http.delete<interview[]>(environment.baseurl+'/api/Interview/company/company-user/'+id+'/cancel')
   }
 }
